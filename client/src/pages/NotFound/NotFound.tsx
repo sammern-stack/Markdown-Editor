86f394dcd89dbb713c2fa5a4aa5787daf08005c4
sammern <samreactdev@gmail.com>
2026-07-03 13:49:01 +0300

    fix: style import for 404 page

--- client/src/pages/NotFound/NotFound.tsx
+++ client/src/pages/NotFound/NotFound.tsx
@@ -1,9 +1,9 @@
 import { Link } from "react-router-dom";
-import "./NotFound.module.scss";
+import styles from "./NotFound.module.scss";
 
 const NotFound = () => {
   return (
-    <div className="notFound">
+    <div className={styles.notFound}>
       <h1>404 Page not found</h1>
       <Link to="/">Go back home</Link>
     </div>
