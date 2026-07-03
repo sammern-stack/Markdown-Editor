ee5b00876e46b315ba2f083a6a82c7fb7d173f00
sammern <samreactdev@gmail.com>
2026-07-03 17:48:14 +0300

    refactor: added Validation and NotFound custom errors and update markdown file name

--- server/src/utils/customErrors.ts
+++ server/src/utils/customErrors.ts
@@ -10,3 +10,15 @@ export class AppError extends Error {
     Error.captureStackTrace(this, this.constructor);
   }
 }
+
+export class ValidationError extends AppError {
+  constructor(message: string) {
+    super(message, 400);
+  }
+}
+
+export class NotFoundError extends AppError {
+  constructor(resource: string) {
+    super(`${resource} not found`, 404);
+  }
+}
