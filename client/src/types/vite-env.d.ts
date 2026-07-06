d6f5936f053b44632c1c87d6249e798525d8f212
sammern <samreactdev@gmail.com>
2026-07-06 12:24:12 +0300

    feat: create axios instance add added vite env variables with types

--- client/src/types/vite-env.d.ts
+++ client/src/types/vite-env.d.ts
@@ -1,3 +1,13 @@
+/// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_AXIOS_BASE_URL: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
+
 declare module "*.svg" {
   import type { FunctionComponent, SVGProps } from "react";
   const ReactComponent: FunctionComponent<SVGProps<SVGSVGElement>>;
