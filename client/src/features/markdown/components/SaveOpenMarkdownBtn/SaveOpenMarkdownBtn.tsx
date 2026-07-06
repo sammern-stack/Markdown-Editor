3f4ca3a202fa0f307e110d1060aa2df911e2727a
sammern <samreactdev@gmail.com>
2026-07-06 14:42:46 +0300

    feat: create save function to save open document information and clear out the open markdown state

--- client/src/features/markdown/components/SaveOpenMarkdownBtn/SaveOpenMarkdownBtn.tsx
+++ client/src/features/markdown/components/SaveOpenMarkdownBtn/SaveOpenMarkdownBtn.tsx
@@ -1,9 +1,18 @@
 import styles from "./SaveOpenMarkdownBtn.module.scss";
 import { Icon } from "@/shared/components";
+import { useOpenMarkdownStore } from "../../stores/openMarkdownStore";
+import { createMarkdownFile } from "../../services/markdownApi";
 
 export const SaveOpenMarkdownBtn = () => {
+  const content = useOpenMarkdownStore((s) => s.markdownContent);
+  const name = useOpenMarkdownStore((s) => s.markdownTitle);
+  const clearContent = useOpenMarkdownStore((s) => s.clearMarkdownContent);
+  const clearTitle = useOpenMarkdownStore((s) => s.clearMarkdownTitle);
+
   const handleSave = () => {
-    // Handle saving the current open document changes from here
+    createMarkdownFile({ name, content });
+    clearContent();
+    clearTitle();
   };
 
   return (
