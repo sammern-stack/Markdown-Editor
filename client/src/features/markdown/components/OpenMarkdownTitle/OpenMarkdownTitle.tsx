c9520e2540fec90c40c350b95082d4bc3a020ee8
sammern <samreactdev@gmail.com>
2026-07-06 14:41:53 +0300

    feat: implement a zustand state to update markdown title and refactor input to handle it

--- client/src/features/markdown/components/OpenMarkdownTitle/OpenMarkdownTitle.tsx
+++ client/src/features/markdown/components/OpenMarkdownTitle/OpenMarkdownTitle.tsx
@@ -1,15 +1,23 @@
 import styles from "./OpenMarkdownTitle.module.scss";
 import { Icon } from "@/shared/components";
+import { useOpenMarkdownStore } from "../../stores/openMarkdownStore";
 
 export const OpenMarkdownTitle = () => {
+  const markdownTitle = useOpenMarkdownStore((s) => s.markdownTitle);
+  const setMarkdownTitle = useOpenMarkdownStore((s) => s.setMarkdownTitle);
+
   return (
     <div className={styles.openMarkdownTitle}>
       <Icon icon="icon-document" />
       <div className={styles.openMarkdownTitle__content}>
         <p>Document Name</p>
-        {/* // TODO: updated when you create useMarkdownStore */}
         <div className={styles.openMarkdownTitle__input}>
-          <input type="text" value={"welcome.md"} />
+          <input
+            type="text"
+            placeholder="Enter document name..."
+            value={markdownTitle}
+            onChange={(e) => setMarkdownTitle(e.target.value)}
+          />
         </div>
       </div>
     </div>
--- client/src/features/markdown/stores/openMarkdownStore.ts
+++ client/src/features/markdown/stores/openMarkdownStore.ts
@@ -1,12 +1,20 @@
 import { create } from "zustand";
 
 interface OpenMarkdownStore {
+  markdownTitle: string;
+  setMarkdownTitle: (title: string) => void;
+  clearMarkdownTitle: () => void;
+
   markdownContent: string[];
   setMarkdownContent: (content: string[]) => void;
   clearMarkdownContent: () => void;
 }
 
 export const useOpenMarkdownStore = create<OpenMarkdownStore>((set) => ({
+  markdownTitle: "",
+  setMarkdownTitle: (title) => set({ markdownTitle: title }),
+  clearMarkdownTitle: () => set({ markdownTitle: "" }),
+
   markdownContent: [],
   setMarkdownContent: (content) => set({ markdownContent: content }),
   clearMarkdownContent: () => set({ markdownContent: [] }),
